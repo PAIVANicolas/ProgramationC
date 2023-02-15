9229e19e50746291e7165106641b587449db0136
PAIVANicolas-3il <75265504+PAIVANicolas@users.noreply.github.com>
2023-02-15 16:03:46 +0100

    add

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-#define EXO4
+#define PRINTX printf("%d\n",x)
+#define PRINT(int) printf("%d\n", int)
+#define PRINT(int) printf(#int "= %d\n", int)
+#define PRINT(int) printf(#int "= %d\n", int) 
+
+#define EXO1
 
 int main() {
 
@@ -64,12 +69,80 @@ int main() {
 
 #ifdef EXO4
 
+   char c;
 
+    // Demande à l'utilisateur d'entrer un caractère
+    printf("Entrez un caractere : ");
+    scanf("%c", &c);
 
+    // Affiche le code ASCII du caractère
+    printf("Le code ASCII de %c est %d.\n", c, c);
 
 #endif
 
 #ifdef EXO5
+
+   int x;
+
+   x = - 3 + 4 * 5 - 6 ; printf ("%d\n",x);
+   x = 3 + 4 % 5 - 6 ; printf ("%d\n",x);
+   x = - 3 + 4 % -6 / 5 ; printf ("%d\n",x);
+   x = (7 + 6 ) % 5 / 2 ; printf ("%d\n",x); 
+#endif
+
+#ifdef EXO6
+   int x = 2, y, z ;
+
+   x *= 3 + 2 ; PRINTX;
+   x *= y = z = 4 ;
+   x = y == z ; PRINTX;
+   x == (y = z) ; PRINTX;
+
+#endif
+
+#ifdef EXO7
+   int x, y, z;
+   x = 2 ; y = 1; z = 0;
+   x = x && y || z;
+   PRINT(x);
+   PRINT(x || ! y && z);
+   x = y = 1;
+   z = x ++ - 1; PRINT(x) ; PRINT(z);
+   z += - x ++ + ++ y; PRINT(x) ; PRINT(z);
+   z = x / ++ x; PRINT(z);
+
+#endif
+
+#ifdef EXO8
+   int x, y, z;
+
+   x = 03 ; y = 02 ; z = 01 ;
+   PRINT( x | y & z);
+   PRINT( x | y & ~z);
+   PRINT( x ^ y & ~z);
+   PRINT( x & y && z);
+   x= 1 ; y= -1;
+   PRINT( !x | x);
+   PRINT( ~x | x);
+   PRINT( x ^ x);
+   x <<= 3 ; PRINT(x);
+   y <<= 3 ; PRINT(y);
+   y >>= 3 ; PRINT(y); 
+#endif
+
+#ifdef EXO9
+
+   int x=1, y=1, z=1;
+
+   x += y += z;
+   PRINT (x < y ? y : x);
+   PRINT(x < y ? x ++ : y ++) ;
+   PRINT(x) ; PRINT(y);
+   PRINT(z += x < y ? x ++ : y ++);
+   PRINT(y) ; PRINT(z);
+   x=3 ; y=z=4;
+   PRINT( (z>= y >=x) ? 1 : 0);
+   PRINT( z >= y && y >= x);
 
 #endif
 
