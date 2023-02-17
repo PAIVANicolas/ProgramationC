41f56f91bb99b364189be7ca097c3c6b7a5bb9ff
PAIVANicolas-3il <75265504+PAIVANicolas@users.noreply.github.com>
2023-02-17 09:42:08 +0100

    Exo2 fini

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 
-#define EXO1
+#define EXO2
+
 
 int main() {
 
-   // printf("Hello, World! \n");
-
 #ifdef EXO1
+
+// a) Ecrire un programme qui lit la dimension N d'un tableau T du type int (dimension maximale: 50
+// composantes), remplit le tableau par des valeurs entrées au clavier et affiche le tableau.
+// b) Effacer ensuite toutes les occurrences de la valeur 0 dans le tableau T et "tasser" les éléments
+// restants. Afficher le tableau résultant, puis afficher ensuite la taille totale réservée pour le tableau
+// en octets puis la taille de la partie occupée du tableau. 
+
    int n;
    int tableau[50];
 
@@ -53,6 +59,32 @@ int main() {
 #endif
 
 #ifdef EXO2
+//Réutilisez le tableau précédant (2.1 a) dans lequel vous rangerez les éléments du tableau T dans l'ordre
+// inverse de saisie sans utiliser de tableau d'aide. Afficher le tableau résultant. 
+
+   int tailleTableau;
+   int tableau[50];
+
+   printf("Entrez la dimension du tableau (maximum 50) : ");
+   scanf("%d", &tailleTableau);
+
+   if (tailleTableau > 50) {
+      printf("Erreur : dimension trop grande.\n");
+      return 1;
+   }
+
+   // Remplissage du tableau
+   for (int i = 0; i < tailleTableau; i++) {
+      printf("Entrez un nombre : ");
+      scanf("%d", &tableau[i]);
+   }
+
+   // Affichage du tableau résultant
+   printf("Voici votre tableau avec les valeurs dans le sens inverse : ");
+   for (int i = tailleTableau - 1; i >= 0; i--) {
+      printf("%d ", tableau[i]);
+   }
+   printf("\n");
 
 
 #endif
